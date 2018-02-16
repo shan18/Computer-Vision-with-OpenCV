ef0d909aa30e3d9495cc7880383a0d1ca9241ce5
Shantanu Acharya <thegeek.004@gmail.com>
2018-02-16 13:51:58 +0530

    Add image resizing

---
+++ image_resizing/batch_image_resizing.py
@@ -0,0 +1,10 @@
+import os
+import cv2
+
+input_path = 'input/sample-images/'
+output_path = 'output/'
+img_list = os.listdir(input_path)
+for img in img_list:
+    img_cv2 = cv2.imread(os.path.join(input_path, img), 0)
+    img_resize = cv2.resize(img_cv2, (100, 100))
+    cv2.imwrite(os.path.join(output_path, img), img_resize)
